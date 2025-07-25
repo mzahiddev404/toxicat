248715dcac5950b4b37641f71b88edcb175028c7
Muhammad Zahid <mzahid.dev404@gmail.com>
2025-07-25 13:16:53 -0700

    create Toxin Data File as toxins.js

---
+++ routes/toxins.js
@@ -0,0 +1,22 @@
+[
+  {
+    "name": "Permethrin",
+    "toxic": true,
+    "severity": 5,
+    "symptoms": ["lethargy", "seizures", "tremors"],
+    "description": "Neurotoxic due to liver enzyme deficiency in cats.",
+    "action": "Immediate vet care required.",
+    "source": "https://www.petpoisonhelpline.com/poison/permethrin"
+  },
+
+  {
+    "name": "Yogurt",
+    "toxic": false,
+    "severity": 1,
+    "symptoms": [],
+    "description": "Not toxic. Some cats may have mild GI upset.",
+    "action": "No treatment needed.",
+    "source": ""
+  }
+  
+]
